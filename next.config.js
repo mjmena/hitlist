3e7d97dad03f021666488c5468823fd90b61f0de
mjmena <martinjxm@gmail.com>
2023-02-03 00:34:06 -0500

    migrate next.config.js to correct location

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+  },
+};
