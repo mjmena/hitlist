2f1b913b0dfb828fc2b70db6e6b4115237de3c28
mjmena <martinjxm@gmail.com>
2023-02-03 01:11:17 -0500

    Seperate button and referesh highlighting for new Seeds

--- components/Hitlist.tsx
+++ components/Hitlist.tsx
@@ -1,14 +1,24 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Option } from '../types/Option';
 import Goal from './Goal';
 
 const Hitlist: NextPage<{ hitlist: Option[] }> = ({ hitlist }) => {
+  const seed = useRouter().asPath;
+
   return (
     <main className="flex flex-1 flex-col justify-center px-20">
-      <Link href="/">New Seed</Link>
-      {hitlist.map((goal) => (
-        <Goal key={goal.key} goal={goal} />
+      <Link
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+        href="/"
+      >
+        New Seed
+      </Link>
+      <br />
+      <br />
+      {hitlist.map((goal, index) => (
+        <Goal key={seed + String(index)} goal={goal} />
       ))}
     </main>
   );
